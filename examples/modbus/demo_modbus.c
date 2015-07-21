9ec2a6e3d07c48a4a0d223df37d1260387d8abf3
Pascal JEAN aka epsilonrt <pascal.jean@btssn.net>
2015-07-21 23:21:53 +0200

    Ajout coil et discrete input dans demo_modbus.c

--- examples/modbus/demo_modbus.c
+++ examples/modbus/demo_modbus.c
@@ -1,6 +1,7 @@
 /*
  * FreeModbus Libary: AVR Demo Application
  * Copyright (C) 2006 Christian Walter <wolti@sil.at>
+ * Copyright (C) 2015 Pascal Jean, epsilonRT. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -15,8 +16,6 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- *
- * File: $Id$
  */
 
 /* ----------------------- AVR includes ------------------------------------- */
@@ -25,14 +24,27 @@
 #include <avrio/delay.h>
 /* ----------------------- Modbus includes ---------------------------------- */
 #include <mb.h>
+#include <mbutils.h>
 #include <mbport.h>
 
 /* ----------------------- Defines ------------------------------------------ */
 #define REG_INPUT_START 1
-#define REG_INPUT_NREGS 4
+#define REG_INPUT_NREGS 5
+
+#define REG_HOLD_START  1
+#define REG_HOLD_NREGS  20
+
+#define BIT_COIL_START  1
+#define BIT_COIL_NBITS  16
+
+#define BIT_DISC_START  1
+#define BIT_DISC_NBITS  16
 
 /* ----------------------- Static variables --------------------------------- */
-static USHORT usRegInputBuf[REG_INPUT_NREGS] = { 0, 0x1234, 0x5678, 0x9ABC };
+static USHORT   usRegInputBuf[REG_INPUT_NREGS] = { 0, 0xAABB, 0xCCDD, 0xEEFF, 0x1234 };
+static USHORT   usRegHoldBuf[REG_HOLD_NREGS];
+static UCHAR    ucBitCoilBuf[BIT_COIL_NBITS / 8];
+static UCHAR    ucBitDiscBuf[BIT_DISC_NBITS / 8] = { 0x55, 0xAA };
 
 /* ----------------------- Start implementation ----------------------------- */
 int
@@ -41,8 +53,6 @@ main (void) {
   eMBErrorCode eStatus;
 
   eStatus = eMBInit (MB_RTU, 32, 0, 19200, MB_PAR_EVEN);
-  //eStatus = eMBInit (MB_ASCII, 32, 0, 19200, MB_PAR_EVEN);
-
   eStatus = eMBSetSlaveID (0x34, TRUE, ucSlaveID, 3);
   sei ();
 
@@ -51,10 +61,28 @@ main (void) {
 
   for (;;) {
     (void) eMBPoll ();
-
   }
 }
 
+//------------------------------------------------------------------------------
+// Empile un mot dans le buffer
+UCHAR * pucPushWord (UCHAR * pucRegBuffer, USHORT usWord) {
+
+  *pucRegBuffer++ = (UCHAR) (usWord >> 8);
+  *pucRegBuffer++ = (UCHAR) (usWord & 0xFF);
+  return pucRegBuffer;
+}
+
+//------------------------------------------------------------------------------
+// Dépile un mot du buffer
+UCHAR * pucPullWord (UCHAR * pucRegBuffer, USHORT * usWord) {
+
+  *usWord  = (*pucRegBuffer++) << 8;
+  *usWord |=  *pucRegBuffer++;
+  return pucRegBuffer;
+}
+
+//------------------------------------------------------------------------------
 eMBErrorCode
 eMBRegInputCB (UCHAR * pucRegBuffer, USHORT usAddress, USHORT usNRegs) {
   eMBErrorCode eStatus = MB_ENOERR;
@@ -64,8 +92,7 @@ eMBRegInputCB (UCHAR * pucRegBuffer, USHORT usAddress, USHORT usNRegs) {
        && (usAddress + usNRegs <= REG_INPUT_START + REG_INPUT_NREGS)) {
     iRegIndex = (int) (usAddress - REG_INPUT_START);
     while (usNRegs > 0) {
-      *pucRegBuffer++ = (unsigned char) (usRegInputBuf[iRegIndex] >> 8);
-      *pucRegBuffer++ = (unsigned char) (usRegInputBuf[iRegIndex] & 0xFF);
+      pucRegBuffer = pucPushWord (pucRegBuffer, usRegInputBuf[iRegIndex]);
       iRegIndex++;
       usNRegs--;
     }
@@ -76,23 +103,101 @@ eMBRegInputCB (UCHAR * pucRegBuffer, USHORT usAddress, USHORT usNRegs) {
   else {
     eStatus = MB_ENOREG;
   }
-  delay_ms (1);
   return eStatus;
 }
 
+//------------------------------------------------------------------------------
 eMBErrorCode
 eMBRegHoldingCB (UCHAR * pucRegBuffer, USHORT usAddress, USHORT usNRegs,
                  eMBRegisterMode eMode) {
-  return MB_ENOREG;
+
+  eMBErrorCode    eStatus = MB_ENOERR;
+  int             iRegIndex;
+
+  if ( (usAddress >= REG_HOLD_START)
+       && (usAddress + usNRegs <= REG_HOLD_START + REG_HOLD_NREGS)) {
+
+    usRegInputBuf[0]++;
+    iRegIndex = (int) (usAddress - REG_HOLD_START);
+    while (usNRegs > 0) {
+
+      if (eMode == MB_REG_READ) {
+        // lecture mot
+        pucRegBuffer = pucPushWord (pucRegBuffer, usRegHoldBuf[iRegIndex]);
+      }
+      else {
+        // écriture mot
+        pucRegBuffer = pucPullWord (pucRegBuffer, &usRegHoldBuf[iRegIndex]);
+      }
+      iRegIndex++;
+      usNRegs--;
+    }
+  }
+  else {
+
+    eStatus = MB_ENOREG;
+  }
+
+  return eStatus;
 }
 
+//------------------------------------------------------------------------------
 eMBErrorCode
-eMBRegCoilsCB (UCHAR * pucRegBuffer, USHORT usAddress, USHORT usNCoils,
+eMBRegDiscreteCB (UCHAR * pucByteBuffer, USHORT usBitOffset, USHORT usNBits) {
+  eMBErrorCode eStatus = MB_ENOERR;
+  UCHAR ucValues, ucBitsCount;
+
+  if ( (usBitOffset >= BIT_DISC_START)
+       && (usBitOffset + usNBits <= BIT_DISC_START + BIT_DISC_NBITS)) {
+
+    usBitOffset--; // ModBus -> PDU
+    while (usNBits) {
+      // Recopie des bits par paquet de 8 bits max.
+      ucBitsCount = MIN (8, usNBits);
+      ucValues = xMBUtilGetBits (ucBitDiscBuf, usBitOffset, ucBitsCount);
+      xMBUtilSetBits (pucByteBuffer, usBitOffset, ucBitsCount, ucValues);
+      ucValues ^= 0xFF; // Bascule l'état
+      xMBUtilSetBits (ucBitDiscBuf, usBitOffset, ucBitsCount, ucValues);
+      usNBits -= ucBitsCount;
+      usBitOffset += ucBitsCount;
+    }
+  }
+  else {
+    eStatus = MB_ENOREG;
+  }
+  return eStatus;
+}
+
+//------------------------------------------------------------------------------
+eMBErrorCode
+eMBRegCoilsCB (UCHAR * pucByteBuffer, USHORT usBitOffset, USHORT usNBits,
                eMBRegisterMode eMode) {
-  return MB_ENOREG;
-}
+  eMBErrorCode eStatus = MB_ENOERR;
+  static UCHAR ucValues, ucBitsCount;
 
-eMBErrorCode
-eMBRegDiscreteCB (UCHAR * pucRegBuffer, USHORT usAddress, USHORT usNDiscrete) {
-  return MB_ENOREG;
+  if ( (usBitOffset >= BIT_COIL_START)
+       && (usBitOffset + usNBits <= BIT_COIL_START + BIT_COIL_NBITS)) {
+
+    usBitOffset--; // ModBus -> PDU
+    while (usNBits) {
+      // Recopie des bits par paquet de 8 bits max.
+      ucBitsCount = MIN (8, usNBits);
+      if (eMode == MB_REG_READ) {
+        // lecture bits
+        ucValues = xMBUtilGetBits (ucBitCoilBuf, usBitOffset, ucBitsCount);
+        xMBUtilSetBits (pucByteBuffer, usBitOffset, ucBitsCount, ucValues);
+      }
+      else {
+        // écriture bits
+        ucValues = xMBUtilGetBits (pucByteBuffer, usBitOffset, ucBitsCount);
+        xMBUtilSetBits (ucBitCoilBuf, usBitOffset, ucBitsCount, ucValues);
+      }
+      usNBits -= ucBitsCount;
+      usBitOffset += ucBitsCount;
+    }
+  }
+  else {
+    eStatus = MB_ENOREG;
+  }
+  return eStatus;
 }

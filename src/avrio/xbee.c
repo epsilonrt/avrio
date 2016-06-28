12b6dc83876bbe3868299eb91432b7674114e72d
epsilonrt <epsilonrt@gmail.com>
2016-06-28 20:28:42 +0200

    Ajout reset soft dans iXBeeOpen() si pas de reset hard

--- src/avrio/xbee.c
+++ src/avrio/xbee.c
@@ -4,11 +4,11 @@
  * This file is part of AvrIO.
  *
  * This software is governed by the CeCILL license under French law and
- * abiding by the rules of distribution of free software.  You can  use, 
+ * abiding by the rules of distribution of free software.  You can  use,
  * modify and/ or redistribute the software under the terms of the CeCILL
  * license as circulated by CEA, CNRS and INRIA at the following URL
- * <http://www.cecill.info>. 
- * 
+ * <http://www.cecill.info>.
+ *
  * The fact that you are presently reading this means that you have had
  * knowledge of the CeCILL license and that you accept its terms.
  *
@@ -1232,8 +1232,19 @@ iXBeeOpen (xXBee *xbee, const char * pcDevice, xSerialIos * xIos) {
   if (xbee->serial) {
 
     if (xbee->reset) {
-      
-      vDpWrite(xbee->reset, 1);
+
+      // release le reset hard
+      vDpWrite (xbee->reset, 1);
+    }
+    else {
+
+      // reset soft
+      if (iXBeeSendAt (xbee, XBEE_CMD_RESET_SOFT, NULL, 0) < 0) {
+        
+        iXBeeClose (xbee);
+        return -1;
+      }
+      delay_ms (5000);
     }
     return 0;
   }

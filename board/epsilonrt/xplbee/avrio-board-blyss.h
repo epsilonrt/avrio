7e9c2189cc7d9ddf62a14187a970d8512816808b
epsilonrt <epsilonrt@gmail.com>
2016-08-19 15:13:16 +0200

    Ajout board epsilonrt/xplbee

---
+++ board/epsilonrt/xplbee/avrio-board-blyss.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright © 2016 epsilonRT, All rights reserved.
+ *
+ * This software is governed by the CeCILL license under French law and
+ * abiding by the rules of distribution of free software.  You can  use,
+ * modify and/ or redistribute the software under the terms of the CeCILL
+ * license as circulated by CEA, CNRS and INRIA at the following URL
+ * <http://www.cecill.info>.
+ *
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL license and that you accept its terms.
+ */
+#ifndef _AVRIO_BOARD_BLYSS_H_
+#define _AVRIO_BOARD_BLYSS_H_
+/* ========================================================================== */
+
+/* BLYSS ==================================================================== */
+#include <avr/io.h>
+
+/* constants ================================================================ */
+#define BLYSS_OUT_PORT PORTB
+#define BLYSS_OUT_DDR  DDRB
+#define BLYSS_OUT_BIT  0
+
+/* ========================================================================== */
+#endif /* _AVRIO_BOARD_BLYSS_H_ */
